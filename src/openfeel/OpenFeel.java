bb64f60c0b3a1decd2098cd67db81ec5be0ae16b
Swetha Vishnampet <vgswetha92@gmail.com>
2014-04-02 19:01:54 +0800

    Needle in Haystack search

---
+++ src/openfeel/OpenFeel.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package openfeel;
+
+/**
+ *
+ * @author vgswetha92
+ */
+public class OpenFeel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        char[] haystack="Hi this is Swetha.swetha is a girl";
+        char[] needle;
+        System.out.println("Enter Haystack: ");
+       
+        
+        // TODO code application logic here
+    }
+}
